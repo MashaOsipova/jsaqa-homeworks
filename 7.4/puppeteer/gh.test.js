1624a0d1dacb4c24575c8e86a6b7a472bfa8143a
MashaOsipova <69155009+MashaOsipova@users.noreply.github.com>
2023-04-30 12:24:00 +0300

    Update gh.test.js

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,3 +1,5 @@
+const { TimeoutError } = require("puppeteer");
+
 let page;
 
 beforeEach(async () => {
@@ -16,19 +18,43 @@ describe("Github page tests", () => {
     await page.waitForSelector('h1');
     const title2 = await page.title();
     expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+  }, 60000); 
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", link => link.getAttribute('href') );
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 60000); 
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
-    });
+    }, 60000); 
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Sign up for free")
   });
+});
+describe("Github tabs tests", () => {
+
+  test("Enterprice", async () => {
+    await page.goto("https://github.com/enterprise");
+    await page.waitForSelector('h1');
+    const title1 = await page.title();
+    expect(title1).toEqual('Enterprise · A smarter way to work together · GitHub');
+  }, 6000);
+
+  test("Marketplace", async () => {
+    await page.goto("https://github.com/marketplace");
+    await page.waitForSelector('h1');
+    const title1 = await page.title();
+    expect(title1).toEqual('GitHub Marketplace · to improve your workflow · GitHub');
+  }, 6000);
+
+  test("Plans", async () => {
+    await page.goto("https://github.com/pricing");
+    await page.waitForSelector('h1');
+    const title1 = await page.title();
+    expect(title1).toEqual('Pricing · Plans for every developer · GitHub');
+  }, 6000);
+ 
 });
