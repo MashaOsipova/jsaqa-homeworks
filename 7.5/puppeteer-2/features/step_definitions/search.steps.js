d96f43e918832e3f7b8d4a6e7f1ed04883cba72d
MashaOsipova <69155009+MashaOsipova@users.noreply.github.com>
2023-05-11 11:07:57 +0300

    init

--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -1,74 +1,102 @@
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
+
 const {
-  selectDateTime,
-  orderTickets,
-  checkSeatIsTaken,
-} = require("../../lib/util.js");
-const { setDefaultTimeout } = require("cucumber");
-const { getText } = require("../../lib/commands.js");
-setDefaultTimeout(60 * 1000);
+    Given,
+    When,
+    Then,
+    Before,
+    After
+} = require("cucumber");
+const {
+    clickElement,
+    getText,
+    row1PlaseVip,
+    seats
+} = require("../../lib/commands.js");
+const {
+    days,
+    moviTime
+} = require("../../lib/pageIndex.js");
 
-let tomorrow = "nav.page-nav > a:nth-child(2)"; 
-let oneWeek = "nav.page-nav > a:nth-child(7)"; 
-let movieTime = "[data-seance-id='94']"; 
-let ticketHint = "p.ticket__hint";
-let confirmingText =
-  "Покажите QR-код нашему контроллеру для подтверждения бронирования.";
-
-Before(async function () {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
-  const page = await browser.newPage();
-  this.browser = browser;
-  this.page = page;
+Before({
+    timeout: 60 * 1000
+}, async function() {
+    const browser = await puppeteer.launch({
+        args: ['--disable-setuid-sandbox', '--disable-dev-shm-usage', ],
+        headless: false,
+        slowMo: 50
+    });
+    const page = await browser.newPage();
+    this.browser = browser;
+    this.page = page;
 });
 
-After(async function () {
-  if (this.browser) {
-    await this.browser.close();
-  }
-});
-
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`http://qamid.tmweb.ru/client${string}`, {
-    setTimeout: 20000,
-  });
-});
-
-When("user select {int}-th day and movie", async function (int1) {
-  await selectDateTime(
-    this.page,
-    `nav.page-nav > a:nth-child(${int1})`,
-    movieTime
-  );
-});
-
-When("select and book {int} row and {int} seat", async function (int1, int2) {
-  await orderTickets(this.page, int1, int2);
-});
-
-When(
-  "select and book {int} row and {int},{int},{int} seats",
-  async function (int1, int2, int3, int4) {
-    await orderTickets(this.page, int1, int2, int3, int4);
-  }
-);
-
-When(
-  "sees that {int} row and {int} seat is taken trying select them",
-  async function (int1, int2) {
-    await checkSeatIsTaken(this.page, int1, int2);
-    try {
-      await orderTickets(this.page, int1, int2);
-    } catch (error) {
-      expect(error).to.be.an("error");
-      expect(error.message).to.be.equal("Seat(s) is taken");
+After(async function() {
+    if (this.browser) {
+        await this.browser.close();
     }
-  }
-);
+});
 
+Given("user is on {string} page", {
+    timeout: 30 * 1000
+}, async function(string) {
+    return await this.page.goto(`http://qamid.tmweb.ru${string}`, {
+        setTimeout: 20000,
+    });
+});
+
+When("user chooses by day {string}", {
+    timeout: 60 * 1000
+}, async function(string) {
+    await days(this.page, string);
+});
+
+When("user chooses movie {string} show {string}", async function(string, string2) {
+    return await moviTime(this.page, string, string2);
+});
+
+When("user chooses seat Vip {string}", async function(string) {
+    return await row1PlaseVip(this.page, string);
+});
+
+When("user chooses seat {string}, {string}", async function(string, string2) {
+    return await seats(this.page, string, string2);
+});
+
+When("user click {string}", async function(string) {
+    return await clickElement(this.page, string);
+});
+
+Then("user sees text {string}", async function(string) {
+    const actual = await getText(this.page, ".ticket__check-title");
+    const expected = await string;
+    expect(actual).contains(expected);
+});
+
+Then("user sees the reserved seat {string}", async function(string) {
+    const actual = await getText(this.page, "main > section > div > p:nth-child(2) > span");
+    const expected = await string;
+    expect(actual).contains(expected);
+});
+
+Then("user sees the header {string}", async function(string) {
+    const actual = await getText(this.page, "h2");
+    const expected = await string;
+    expect(actual).contains(expected);
+});
+
+Then("user sees {string} is gray", {
+    timeout: 60 * 1000
+}, async function(string) {
+    await clickElement(this.page, string);
+    await this.page.waitForNavigation(30000);
+    const disabledButton = await page.$('button[disabled]');
+    const isDisabled = await disabledButton !== null;
+    await this.page.waitForNavigation(30000);
+    expect(isDisabled).to.not.be.null;
+});
 Then("user received confirmation and qr-code", async function () {
   const actual = await getText(this.page, ticketHint);
   expect(actual).contain(confirmingText);
